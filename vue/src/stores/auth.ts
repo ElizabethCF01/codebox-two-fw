a6e909322a57f6213f21b5b125bb6a85f0e3d74d
ElizabethCF01 <elizabethcalzadillafernandez@gmail.com>
2025-04-04 07:10:01 +0200

    fix: pinia persist state

--- vue/src/stores/auth.ts
+++ vue/src/stores/auth.ts
@@ -1,18 +1,16 @@
 import type { StrapiUserResponse } from "@/interfaces/user";
 import { defineStore } from "pinia";
-import { type Ref, ref } from "vue";
 
-export const useAuthStore = defineStore(
-  "auth",
-  () => {
-    const user: Ref<StrapiUserResponse | undefined> = ref(undefined);
-    const jwt: Ref<string | undefined> = ref(undefined);
-
-    const logout = () => {
-      user.value = undefined;
-      jwt.value = undefined;
-    };
-    return { user, jwt, logout };
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    user: undefined as StrapiUserResponse | undefined,
+    jwt: undefined as string | undefined,
+  }),
+  actions: {
+    logout() {
+      this.user = undefined;
+      this.jwt = undefined;
+    },
   },
-  { persist: true },
-);
+  persist: true,
+});
