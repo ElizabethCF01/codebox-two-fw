cbd84dcf349bc8c90f1842a861b2397893ca0c6b
ElizabethCF01 <elizabethcalzadillafernandez@gmail.com>
2025-04-04 06:18:36 +0200

    fix: playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,16 +25,11 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     browserName: "chromium",
     headless: true,
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "react",
@@ -46,10 +41,16 @@ export default defineConfig({
     },
   ],
 
-  /* Run your local dev server before starting the tests */
-  //   webServer: {
-  //     command: "npm run dev",
-  //     port: 5173,
-  //     reuseExistingServer: !process.env.CI,
-  //   },
+  webServer: [
+    {
+      command: "npm run dev:react",
+      port: 5173,
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: "npm run dev:vue",
+      port: 5174,
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
 });
